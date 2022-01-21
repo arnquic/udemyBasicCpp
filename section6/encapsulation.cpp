4ed88eaa4ba2a0ce1be3529ed38162bd46c70714
arnquist010 <arnquist010@gmail.com>
2022-01-21 11:00:19 -0700

    Encapsulation. 'Make everything private that you can.'

---
+++ section6/encapsulation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Frog {
+   public:
+    Frog(string name) : name(name) {}
+    void info() { cout << "My name is: " << getName() << endl; }
+
+   private:
+    string name;
+
+   private:
+    string getName() {
+        return this->name;
+    }
+};
+
+int main() {
+    Frog frog("Freddy");
+
+    frog.info();
+
+    return 0;
+}
